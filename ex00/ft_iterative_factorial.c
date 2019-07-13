e197b38a96a82fdadbad86014e39d0d75bc78809
mnenonen@student.hive.fi <mnenonen@c3r3p1.hive.fi>
2019-07-13 16:58:16 +0300

    iterative version

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,18 +6,23 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 16:23:46 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/07/13 16:27:03 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/07/13 16:57:05 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_iterative_factorial(int nb)
 {
-	if (nb == 1)
-		return (1);
-	else if (nb == 0)
-		return (1);
-	else if (nb < 0)
+	int answer;
+	int i;
+
+	answer = 1;
+	i = nb;
+	while (i > 1)
+	{
+		answer = answer * i;
+		--i;
+	}
+	if (nb < 0)
 		return (0);
-	else
-		return (nb * ft_iterative_factorial(nb - 1));
+	return (answer);
 }
