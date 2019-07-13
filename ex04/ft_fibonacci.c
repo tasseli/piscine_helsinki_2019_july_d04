ac7787b77343b3de9a16b852fc852e8033f6f463
mnenonen@student.hive.fi <mnenonen@c3r3p1.hive.fi>
2019-07-13 18:58:55 +0300

    ex04 ok@

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/13 18:46:26 by mnenonen          #+#    #+#             */
+/*   Updated: 2019/07/13 18:58:17 by mnenonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
